7a05b23390afb46976b766cb5dab34451e830dab
WizAom <40514176+WizAom@users.noreply.github.com>
2022-05-08 14:07:12 +0700

    Add files via upload

---
+++ app/config/grid.config.js
@@ -0,0 +1,11 @@
+
+export default {
+    settings: {
+        gridSelector: '#grid',
+        svgSelector: '#gridSvg',
+        cellSize: 15,
+        borderSize: 1,
+        borderColor: '#202020',
+        verticesWeight: 9,
+    }
+}
